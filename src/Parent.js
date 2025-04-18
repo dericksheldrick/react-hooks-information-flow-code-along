660cbd85b4ce92b6098eaab5b24bfd9f54a5e671
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-04-18 15:04:57 +0300

    lifting state and invoking a call back

--- src/Parent.js
+++ src/Parent.js
@@ -4,12 +4,21 @@ import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  const [color, setColor] = useState(randomColor); // intial value of color //lifting states
+  const [childrenColor, setChildrenColor] = useState("#FFF") 
+
+
+  function handleChangeColor(){
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor); // updating the color state to a new value
+    setChildrenColor(newRandomColor);
+  }
 
   return (
+    // passing color state a a prop so that when the child is clicked it works 
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/> 
     </div>
   );
 }
